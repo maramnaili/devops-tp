f3d8b383ed2952f778853871043f6cfc70331d36
maram naili <nailimaram6@gmail.com>
2025-02-28 11:36:13 -1100

    Add hello-devops project with basic app and tests

---
+++ hello-devops/test_app.py
@@ -0,0 +1,7 @@
+import unittest
+# For a simple example , we simply test True .
+class TestApp ( unittest . TestCase ):
+ def test_output ( self ):
+  self . assertTrue ( True )
+if __name__ == " __main__ ":
+ unittest . main ()
