429423c5c4a4282d43ca651629fec3a154f43a7d
maram naili <nailimaram6@gmail.com>
2025-03-02 10:50:28 -1100

    Introduced error for debugging

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, CI/CD!"  
+    return "Hello, CI/CD"  
 
+@app.route('/status')
+def status():
+    return "Status: OK"
 
 if __name__ == "__main__":
     app.run()
